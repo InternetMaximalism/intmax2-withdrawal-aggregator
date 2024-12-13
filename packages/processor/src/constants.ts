f76529a01df02cea67be9f9c59478cd5930ad13c
signature18632 <intmax18@gmail.com>
2024-12-13 07:25:45 +0700

    timeout

--- packages/processor/src/constants.ts
+++ packages/processor/src/constants.ts
@@ -6,13 +6,13 @@ export const DEFAULT_ID_LENGTH = 20;
 // poll
 export const DEFAULT_POLL_OPTIONS: Required<PollOptions> = {
   maxAttempts: 30,
-  intervalMs: 5_000,
+  intervalMs: 6_000,
   timeoutMs: 180_000,
 };
 
 // transaction
-export const REPLACED_TX_WAIT_TIMEOUT = 30_000;
-export const WAIT_TRANSACTION_TIMEOUT = 30_000;
+export const REPLACED_TX_WAIT_TIMEOUT = 15_000;
+export const WAIT_TRANSACTION_TIMEOUT = 15_000;
 export const TRANSACTION_MAX_RETRIES = 5;
 
 // errors

d6e01d070fa559b6f3e442f775200d7500030021
signature18632 <intmax18@gmail.com>
2024-12-29 14:07:18 +0700

    wait for ethers (#14)

--- packages/processor/src/constants.ts
+++ packages/processor/src/constants.ts
@@ -21,3 +21,7 @@ export const TRANSACTION_WAIT_TIMEOUT_ERROR_MESSAGE =
 export const EXECUTION_REVERTED_ERROR_MESSAGE = "Execution reverted";
 export const TRANSACTION_REPLACEMENT_FEE_TOO_LOW = "replacement fee too low";
 export const TRANSACTION_MISSING_REVERT_DATA = "missing revert data"; // because of the gasPrice
+
+// ethers
+export const ETHERS_WAIT_TRANSACTION_TIMEOUT_MESSAGE = "timeout";
+export const ETHERS_CONFIRIMATIONS = 1;

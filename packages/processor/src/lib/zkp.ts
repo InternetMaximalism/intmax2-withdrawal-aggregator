da312ad6c7036451e61c484085470afcc37d7b45
signature18632 <intmax18@gmail.com>
2025-08-12 05:50:05 +0700

    added verifier data type

--- packages/processor/src/lib/zkp.ts
+++ packages/processor/src/lib/zkp.ts
@@ -8,6 +8,7 @@ import type {
   ProverRequestParams,
   WithdrawalProof,
 } from "../types";
+import { getVerifierData } from "./verifierData";
 
 export const createWithdrawalProof = async (
   withdrawalHash: string,
@@ -42,11 +43,14 @@ export const createWrappedProof = async (
 };
 
 export const createGnarkProof = async (wrappedProof: string) => {
+  const verifierData = getVerifierData("withdrawal", "dev");
+
   return makeProverRequest<CreateGnarkProofResponse>({
     method: "post",
     path: "withdrawal-gnark-server/start-proof",
     data: {
       proof: wrappedProof,
+      verifierData,
     },
   });
 };

95a2fa135661f1f21c27d60b370e40dbb8b3122c
signature18632 <intmax18@gmail.com>
2025-08-24 11:52:04 +0700

    fix rpc

--- packages/processor/src/service/submit.service.ts
+++ packages/processor/src/service/submit.service.ts
@@ -122,7 +122,7 @@ export const submitWithdrawalProofWithRetry = async (
     gasPrice,
   };
 
-  const provider = new ethers.JsonRpcProvider(l2Client.transport.url);
+  const provider = new ethers.JsonRpcProvider(l2Client.transport.transports[0].value.url);
   const signer = new ethers.Wallet(
     toHex(walletClientData.account.getHdKey().privateKey!),
     provider,
